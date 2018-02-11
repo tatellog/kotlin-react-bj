5e84c685c318407dbb535e31b2539cb27d99d507
ttello <ttello@adquem.com>
2018-02-10 22:58:06 -0600

    style was added

--- src/index/index.kt
+++ src/index/index.kt
@@ -1,20 +1,39 @@
+@file:Suppress("UnsafeCastFromDynamic")
+
 package index
 
+import kotlinext.js.js
+import kotlinx.html.style
+import kotlinx.html.title
 import react.dom.*
 import kotlin.browser.*
 
 fun main(args: Array<String>) {
     val rootDiv = document.getElementById("root")
     render(rootDiv) {
-        h1 {
-            +"Black Jack"
+        div {
+            h1 { +"Black Jack"}
             div {
-                div { b { + "Player Hand" } }
-                div { b { + "Cards goes Here" } }
-                div { b { + "12 points" } }
+
+                attrs.title = "Foo"
+                attrs.style = js {
+                    display = "flex"
+                }
+                div {
+                    div { b { + "Player Hand" } }
+                    div {  + "Cards goes Here" }
+                    div { b { + "12 points" } }
+                }
+
+                div {
+                    div { b { + "Dealer Hand" } }
+                    div { + "Cards goes Here"  }
+                    div { b { + "14 points" } }
+                }
             }
 
+            }
         }
 
-    }
+
 }
