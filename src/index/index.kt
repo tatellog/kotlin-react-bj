b5814f5a02c768d9fcd775746fecf49a64bc85da
ttello <ttello@adquem.com>
2018-02-10 22:31:37 -0600

    add divs

--- src/index/index.kt
+++ src/index/index.kt
@@ -6,7 +6,15 @@ import kotlin.browser.*
 fun main(args: Array<String>) {
     val rootDiv = document.getElementById("root")
     render(rootDiv) {
-        h1 { +"Black Jack" }
+        h1 {
+            +"Black Jack"
+            div {
+                div { b { + "Player Hand" } }
+                div { b { + "Cards goes Here" } }
+                div { b { + "12 points" } }
+            }
+
+        }
 
     }
 }
